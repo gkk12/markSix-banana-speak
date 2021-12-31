1cf14a0ae6282320a3349e78d3507528e7364c05
Gautham Kamath K <gauthamkamathk24@gmail.com>
2021-12-31 17:02:24 +0530

    taking changes from the user

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+btnTranslate.addEventListener("click", clickHandler)
+
+function clickHandler() {
+        
+        console.log(txtInput.value);
+};
